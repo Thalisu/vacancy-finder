e1c96fef6813b3d9c73dd075678c832a3bc170a2
Thalisu <thalisondmcc@gmail.com>
2024-10-04 00:47:00 -0300

    feat: add home page keywordField form

---
+++ app/(home)/Form.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState, ReactNode } from "react";
+import KeywordField from "../components/KeywordField";
+
+export default function KeywordForm() {
+  const [searchs, setSearchs] = useState<ReactNode[]>([
+    <KeywordField key={0} index={1} />,
+  ]);
+  return (
+    <form className="flex w-fit min-w-[50%] max-w-[80%] flex-col gap-2 rounded-md bg-gray-900 p-4">
+      <div className="flex w-full flex-col gap-4">
+        {searchs.map((search) => search)}
+      </div>
+      <button
+        type="button"
+        className="flex w-fit max-w-fit items-center justify-center gap-2 rounded-xl bg-gray-950 p-3"
+        onClick={() =>
+          setSearchs((prev) => [
+            ...prev,
+            <KeywordField key={prev.length} index={prev.length + 1} />,
+          ])
+        }
+      >
+        Mais pesquisas
+      </button>
+    </form>
+  );
+}
