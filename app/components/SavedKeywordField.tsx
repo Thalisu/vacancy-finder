db176e5764fbed9baa8becb4ca0ade3fc0c7c720
Thalisu <thalisondmcc@gmail.com>
2024-10-07 14:34:40 -0300

    feat: add savedKeywordField

--- app/components/SavedKeywordField.tsx
+++ app/components/SavedKeywordField.tsx
@@ -1,13 +1,30 @@
 import { inter } from "../lib/fonts";
 
-export default function SavedKeywordField({ value }: { value: string }) {
+export default function SavedKeywordField({
+  values,
+  handler,
+}: {
+  values: string[];
+  handler: (value: string, index: number) => void;
+}) {
+  const isKeyword = values.map((v) => {
+    const is = v === "AND" || v === "OR" || v === "(" || v === ")";
+    return !is;
+  });
   return (
-    <input
-      type="text"
-      disabled
-      value={value}
-      name="keyword"
-      className={`${inter.className} w-full rounded-md bg-gray-800 p-1 text-center`}
-    />
+    <div className="flex flex-wrap">
+      {values.map((v, i) => (
+        <input
+          type="text"
+          readOnly
+          value={v}
+          name="keyword"
+          className={`${inter.className} ${i === 0 && "rounded-s-md"} ${i === values.length - 1 && "rounded-e-md"} cursor-default bg-gray-800/50 py-1 text-center outline-none transition-colors ${isKeyword[i] && "!cursor-pointer hover:bg-gray-800"}`}
+          onClick={() => isKeyword[i] && handler(v, i)}
+          style={{ width: `${v.length * 12}px` }}
+          key={i}
+        />
+      ))}
+    </div>
   );
 }
