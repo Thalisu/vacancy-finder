8b20b2272d2d41843ba88d05f6db3c0830dcd0c4
Thalisu <thalisondmcc@gmail.com>
2024-10-08 18:36:35 -0300

    feat: add select component

---
+++ app/components/Select.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+
+export default function Select({
+  id,
+  label,
+  index,
+  children,
+  selectConfig = {},
+}: {
+  id: string;
+  label: string;
+  index: number;
+  children: ReactNode;
+  selectConfig?: Record<string, unknown>;
+}) {
+  return (
+    <div>
+      <label htmlFor={`${id}-${index}`}>{label}</label>
+      <select
+        id={`${id}-${index}`}
+        {...selectConfig}
+        className={`max-w-fit rounded-md ${selectConfig.disabled ? "bg-gray-800/50" : "bg-gray-800"} px-2 py-1`}
+        name={`${id}-${index}`}
+      >
+        {children}
+      </select>
+    </div>
+  );
+}
