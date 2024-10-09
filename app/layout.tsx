682ba74c56abf3b9046117499e9b3d42670ead8e
Thalisu <thalisondmcc@gmail.com>
2024-10-09 18:19:17 -0300

    feat: add JobDataProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "./lib/fonts";
+import JobDataProvider from "./provider/JobData.provider";
 
 export const metadata: Metadata = {
   title: "Vacancy Finder",
@@ -16,9 +17,11 @@ export default function RootLayout({
       <body
         className={`${inter.className} flex min-h-svh w-svw flex-col bg-gray-950/95 antialiased`}
       >
-        <main className="mx-2 flex flex-1 flex-col items-center justify-center">
-          {children}
-        </main>
+        <JobDataProvider>
+          <main className="mx-2 flex flex-1 flex-col items-center justify-center">
+            {children}
+          </main>
+        </JobDataProvider>
       </body>
     </html>
   );
