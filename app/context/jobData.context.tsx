7898196871d226b5cf48f883aa5ca057c04f0d3c
Thalisu <thalisondmcc@gmail.com>
2024-10-09 16:21:07 -0300

    feat: add data context to handle jobs in the job page

---
+++ app/context/jobData.context.tsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const jobDataContext = createContext({});
+
+export default jobDataContext;
---
+++ app/provider/JobData.provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import jobDataContext from "../context/jobData.context";
+import { IFormState } from "../lib/types";
+
+export default function JobDataProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [data, setData] = useState<IFormState>();
+  const handleSetData = (data: IFormState) => {
+    //Todo data type validation
+    setData(() => data);
+  };
+  return (
+    <jobDataContext.Provider value={{ data, handleSetData }}>
+      {children}
+    </jobDataContext.Provider>
+  );
+}
