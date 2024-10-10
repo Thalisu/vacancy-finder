66bb2a0a67ad55686738c3cc68c9556dc7ef039a
Thalisu <thalisondmcc@gmail.com>
2024-10-10 01:12:33 -0300

    feat: add type checking to jobDataContext

--- app/context/jobData.context.tsx
+++ app/context/jobData.context.tsx
@@ -1,5 +1,17 @@
 import { createContext } from "react";
+import { IFormState, IFormStateData } from "../lib/types";
 
-const jobDataContext = createContext({});
+const jobDataContext = createContext<{
+  data: IFormState;
+  handleSetData: (data: IFormStateData) => void;
+}>({
+  data: {
+    length: 0,
+    jobs: [],
+    errors: [],
+    loading: true,
+  },
+  handleSetData: () => {},
+});
 
 export default jobDataContext;
