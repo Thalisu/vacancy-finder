63e5d510dc2e4177ef3b57ef409612fa2040e1de
Thalisu <thalisondmcc@gmail.com>
2024-10-10 18:38:11 -0300

    feat: redirect if no form submited

--- app/(pages)/jobs/jobContainer.tsx
+++ app/(pages)/jobs/jobContainer.tsx
@@ -2,10 +2,14 @@
 import { JobCard } from "@/app/components/JobCard";
 import jobDataContext from "@/app/context/jobData.context";
 import { IFormStateData } from "@/app/lib/types";
+import { redirect } from "next/navigation";
 import { useContext } from "react";
 
 export default function JobContainer() {
   const { data } = useContext(jobDataContext);
+  if (data.loading) {
+    redirect("/");
+  }
   return (
     <ul className="flex flex-col gap-2">
       {data.jobs.map((searchs) =>
