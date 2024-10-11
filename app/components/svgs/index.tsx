a126eafcea5df8a327163b1e798a95500bb47017
Thalisu <thalisondmcc@gmail.com>
2024-10-11 17:13:18 -0300

    feat: add copy and edit svgs

--- app/components/svgs/index.tsx
+++ app/components/svgs/index.tsx
@@ -1,3 +1,5 @@
 export { default as PlusSVG } from "@/app/components/svgs/plus.svg";
 export { default as MinusSVG } from "@/app/components/svgs/minus.svg";
 export { default as HeaderSVG } from "@/app/components/svgs/header.svg";
+export { default as EditSVG } from "@/app/components/svgs/edit.svg";
+export { default as CopySVG } from "@/app/components/svgs/copy.svg";
