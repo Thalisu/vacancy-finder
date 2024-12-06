73f6bb34a763960a8c4d50f85c6b1c549b18b8aa
Thalisu <thalisondmcc@gmail.com>
2024-12-06 15:10:19 -0300

    feat: use state on keyword input

--- app/components/KeywordInput.tsx
+++ app/components/KeywordInput.tsx
@@ -1,12 +1,9 @@
+"use client";
+import { useState } from "react";
 import { inter } from "../lib/fonts";
 
-export default function KeywordInput({
-  value,
-  handleChange,
-}: {
-  value: string;
-  handleChange: (newValue: string) => void;
-}) {
+export default function KeywordInput() {
+  const [value, setValue] = useState("");
   const resize = (input: EventTarget & HTMLInputElement) => {
     if (input.scrollWidth > input.clientWidth) {
       input.style.width = `${input.scrollWidth}px`;
@@ -15,13 +12,14 @@ export default function KeywordInput({
 
   return (
     <input
+      id="keyword"
       type="text"
       maxLength={15}
       required
       value={value}
       placeholder="keyword"
       onChange={({ currentTarget }) => {
-        handleChange(currentTarget.value);
+        setValue(currentTarget.value);
         resize(currentTarget);
       }}
       className={`${inter.className} h-full w-28 rounded-md bg-gray-800 p-1 text-center`}
