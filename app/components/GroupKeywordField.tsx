b599d20375f95100ff1ac72daae25d85eb4c4e4d
Thalisu <thalisondmcc@gmail.com>
2024-12-06 15:09:34 -0300

    perf: dont re-render all fields when changing one state

--- app/components/GroupKeywordField.tsx
+++ app/components/GroupKeywordField.tsx
@@ -1,65 +1,54 @@
 "use client";
-import { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { MinusSVG, PlusSVG } from "./svgs";
 import { inter } from "../lib/fonts";
 import KeywordInput from "./KeywordInput";
 import SelectAndOr from "./SelectAndOr";
+import ExtraField from "./SelectAndKeyField";
 
-export default function GroupKeywordField({
-  value,
-  handleValueChange,
-}: {
-  value: string[];
-  handleValueChange: (newValue: string | string[]) => void;
-}) {
-  const [val, setVal] = useState<string[]>([...value]);
+export default function GroupKeywordField() {
+  const [extraFields, setExtraFields] = useState<ReactNode[]>([]);
 
-  const handleChange = (index: number, newValue: string) => {
-    const toUpdate = [...val];
-    toUpdate[index] = newValue;
-    setVal(() => [...toUpdate]);
-    handleValueChange(toUpdate);
+  const addExtraField = () => {
+    setExtraFields((prev) => [
+      ...extraFields,
+      <ExtraField key={prev.length} />,
+    ]);
   };
 
-  const addKeyword = () => {
-    const toUpdate = [...val, "AND", ""];
-    setVal(() => toUpdate);
-    handleValueChange(toUpdate);
-  };
-
-  const removeKeyword = () => {
-    const toUpdate = [...val.slice(0, -2)];
-    setVal(() => toUpdate);
-    handleValueChange(toUpdate);
+  const removeExtraField = () => {
+    if (extraFields.length < 1) return;
+    setExtraFields((prev) => extraFields.slice(0, prev.length - 1));
   };
 
   return (
     <div className={`${inter.className} flex flex-wrap items-center gap-2`}>
-      <p className="text-3xl text-blue-900/70">{"("}</p>
-      {val.map((val, i) => {
-        return i % 2 === 0 ? (
-          <KeywordInput
-            key={i}
-            value={val}
-            handleChange={(newValue) => handleChange(i, newValue)}
-          />
-        ) : (
-          <SelectAndOr
-            key={i}
-            value={val}
-            handleChange={(newValue) => handleChange(i, newValue)}
-          />
-        );
-      })}
+      <input
+        id="keyword"
+        className="pointer-events-none w-2 cursor-default bg-transparent text-3xl text-blue-900/70"
+        value={"("}
+        readOnly
+      />
+      <KeywordInput />
+      <SelectAndOr />
+      <KeywordInput />
+      {extraFields.length > 0 && extraFields.map((ef: ReactNode) => ef)}
       <div className="flex flex-col gap-1">
-        <button type="button" onClick={() => addKeyword()}>
+        <button type="button" onClick={() => addExtraField()}>
           <PlusSVG />
         </button>
-        <button type="button" onClick={() => removeKeyword()}>
-          <MinusSVG />
+        <button type="button" onClick={() => removeExtraField()}>
+          <MinusSVG
+            className={`${extraFields.length < 1 ? "opacity-50" : ""}`}
+          />
         </button>
       </div>
-      <p className="text-3xl text-blue-900/70">{")"}</p>
+      <input
+        id="keyword"
+        className="pointer-events-none w-2 cursor-default bg-transparent text-3xl text-blue-900/70"
+        value={")"}
+        readOnly
+      />
     </div>
   );
 }
