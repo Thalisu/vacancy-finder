5375ad6efb2041171d9fd3a564834f2cef25ced9
Thalisu <thalisondmcc@gmail.com>
2024-12-06 15:10:01 -0300

    refactor: better way to handle keyword fields

--- app/components/KeywordField.tsx
+++ app/components/KeywordField.tsx
@@ -1,31 +1,38 @@
 "use client";
+import { ReactNode, useCallback, useRef, useState } from "react";
 import { inter } from "../lib/fonts";
-import useKeywordInputs from "../hooks/useKeywordInputs";
-import { Dispatch, SetStateAction } from "react";
-import SavedSearchButtons from "./SavedSearchButtons";
+import NewKeywordSearch from "./NewKeywordSearch";
 import NotSavedSearchButtons from "./NotSavedSearchButtons";
-import JobDataSelects from "./JobDataSelects";
+import SavedKeywordField from "./SavedKeywordField";
 
-export default function KeywordField({
-  index,
-  setIsSearchAvailable,
-}: {
-  index: number;
-  setIsSearchAvailable: Dispatch<SetStateAction<boolean>>;
-}) {
-  const {
-    keywordInputs,
-    isAllInputsWithValue,
-    addKeyword,
-    removeKeyword,
-    saveSearch,
-    isSaved,
-    jobSearchData,
-    handleSetJobSearchData,
-    editSearch,
-  } = useKeywordInputs(setIsSearchAvailable, index);
+export default function KeywordField({ index }: { index: number }) {
+  const [isSaved, setIsSaved] = useState(false);
+  const [extraFields, setExtraFields] = useState<ReactNode[]>([]);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const isLengthOne = keywordInputs.length === 1;
+  const addExtraField = useCallback(() => {
+    setExtraFields((prev) => [...prev, <NewKeywordSearch key={prev.length} />]);
+  }, []);
+
+  const removeExtraField = useCallback(() => {
+    if (extraFields.length < 1) return;
+    setExtraFields(() => extraFields.slice(0, extraFields.length - 1));
+  }, [extraFields]);
+
+  const handleSave = () => {
+    setIsSaved((prev) => !prev);
+  };
+
+  if (isSaved && ref.current) {
+    const elementsArray = Array.from(
+      ref.current.querySelectorAll("#keyword") || [],
+    );
+    console.log(elementsArray);
+    const values = elementsArray.map((i) => (i as HTMLInputElement).value);
+    return (
+      <SavedKeywordField handler={handleSave} values={values} index={index} />
+    );
+  }
 
   return (
     <div className="relative flex min-h-32 flex-col gap-2 bg-gray-950/30 p-4">
@@ -34,31 +41,16 @@ export default function KeywordField({
       </p>
       <div
         className={`${inter.className} mb-auto flex flex-wrap items-center gap-2`}
+        ref={ref}
       >
-        {keywordInputs.map((input) => input.node)}
+        <NewKeywordSearch label />
+        {extraFields.length > 0 && extraFields.map((ef: ReactNode) => ef)}
       </div>
-
-      {isSaved ? (
-        <SavedSearchButtons
-          values={keywordInputs.map((i) => i.value as string[]).flat()}
-          editSearch={editSearch}
-        />
-      ) : (
-        <>
-          <JobDataSelects
-            index={index}
-            jobSearchData={jobSearchData}
-            handleSetJobSearchData={handleSetJobSearchData}
-          />
-          <NotSavedSearchButtons
-            isAllInputsWithValue={isAllInputsWithValue}
-            isLengthOne={isLengthOne}
-            addKeyword={addKeyword}
-            removeKeyword={removeKeyword}
-            saveSearch={saveSearch}
-          />
-        </>
-      )}
+      <NotSavedSearchButtons
+        addExtraFields={addExtraField}
+        removeExtraField={removeExtraField}
+        handleSave={handleSave}
+      />
     </div>
   );
 }
