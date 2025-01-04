d838b555f87976731c35b2af33cf59faf9f6f823
Thalisu <thalisondmcc@gmail.com>
2025-01-04 02:49:25 -0300

    refactor: change Header

--- frontend/app/(pages)/jobs/page.tsx
+++ frontend/app/(pages)/jobs/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/app/components/Header";
 import JobContainer from "./jobContainer";
 
 export default function Jobs() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full grow flex-col items-center">
+      <Header small={true} />
       <JobContainer />
     </div>
   );
--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,9 +1,11 @@
 import { HeaderSVG } from "./svgs";
 
-export default function Header() {
+export default function Header({ small = false }: { small?: boolean }) {
   return (
-    <header className="flex w-full justify-center py-6">
-      <HeaderSVG />
+    <header className="flex h-fit w-full justify-center py-4">
+      <HeaderSVG
+        className={`${small ? "w-1/5" : "w-3/5"} h-auto min-w-96 max-w-2xl`}
+      />
     </header>
   );
 }
