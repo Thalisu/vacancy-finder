b1c6b58da625ddf345ce1a0f1b9ed9c74976d638
Thalisu <thalisondmcc@gmail.com>
2025-01-04 02:44:07 -0300

    style: add loading animations

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,11 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       backgroundColor: {
-        neutral: "var(--background-color)",
         primaryForm: "var(--primary-form-color)",
         secondaryForm: "var(--secondary-form-color)",
       },
       colors: {
+        neutral: "var(--background-color)",
         primary: "var(--primary-color)",
         complementary: "var(--complementary-color)",
         accent: "var(--accent-color)",
@@ -24,6 +24,34 @@ const config: Config = {
         light: "var(--lighter)",
         success: "var(--success)",
         error: "var(--error)",
+      },
+      animation: {
+        loading: "loading 2s infinite ease",
+        loadingInner: "loadingInner 2s infinite ease",
+      },
+      keyframes: {
+        loading: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "25%,50%": {
+            transform: "rotate(180deg)",
+          },
+          "75%,100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        loadingInner: {
+          "0%,25%": {
+            height: "0%",
+          },
+          "50%,75%": {
+            height: "100%",
+          },
+          "100%": {
+            height: "0%",
+          },
+        },
       },
     },
   },
