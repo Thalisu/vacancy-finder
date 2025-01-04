c7280347276ba286cfc5382d800d64f404c6a914
Thalisu <thalisondmcc@gmail.com>
2025-01-04 18:38:35 -0300

    chore: update type

--- frontend/app/hooks/useKeywordFields.tsx
+++ frontend/app/hooks/useKeywordFields.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useRef, useState } from "react";
-import { IJobResponse } from "../lib/types";
+import { IJobsData } from "../lib/types";
 import NewKeywordSearch from "../components/search/NewKeywordSearch";
 import { addToLocalStorage, updateLocalStorage } from "../lib/utils";
 
@@ -10,7 +10,7 @@ const useKeywordFields = (
   },
   searchIndex: number,
   savedSearch?: {
-    jobsData: IJobResponse;
+    jobsData: IJobsData;
     keywords: string[];
   },
 ) => {
